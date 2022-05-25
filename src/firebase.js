5240006189b1980f87634d18df2a414ac51630f5
devlunch4 <devlunch4@gmail.com>
2022-05-25 11:36:02 +0900

    firebase setup 01

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import firebase from "firebase/compat/app";
+import * as firebase from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBx5ogWPGspFfZO3-lWNC837gdK8Q1hDaE",
+    authDomain: "nwitter-cc2ce.firebaseapp.com",
+    projectId: "nwitter-cc2ce",
+    storageBucket: "nwitter-cc2ce.appspot.com",
+    messagingSenderId: "976678443467",
+    appId: "1:976678443467:web:4866d3abd9506a631a3e4e",
+    measurementId: "G-0NMJFET60Y"
+  };
+
+export default firebaseConfig.initializeApp(firebaseConfig);
