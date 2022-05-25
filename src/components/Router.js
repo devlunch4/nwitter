e208f4b6d23f7736b113037265f66264280232e3
devlunch4 <devlunch4@gmail.com>
2022-05-25 13:21:20 +0900

    #1.2 Router Setup (04:30)

---
+++ src/components/Router.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { HashRouter as Router, Rount, Switch } from "react-router-dom";
+
+export default () => <Router>
+    <Switch>
+        
+    </Switch>
+</Router>
