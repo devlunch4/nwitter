9e8d88a146dab227c1a5cc5d824e1033cbb99e4f
devlunch4 <devlunch4@gmail.com>
2022-05-25 13:47:50 +0900

    #1.2 Router Setup (09:59)

--- src/index.js
+++ src/index.js
@@ -3,16 +3,16 @@ import ReactDOM from 'react-dom/client';
 import App from './components/App';
 
 
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
 
-ReactDOM.render(   <React.StrictMode>
-     <App />
-   </React.StrictMode>,
-   document.getElementById("root")
-   )
+// ReactDOM.render(   <React.StrictMode>
+//      <App />
+//    </React.StrictMode>,
+//    document.getElementById("root")
+//    )
